67e229f030d6a673eddafb03cff578b114807a0b
maia-d <ac7642@merrillwireless145-164.hampshire.edu>
2017-03-24 00:16:19 -0400

    Still having problem with Volumef and Fade

---
+++ Assets/Scripts/Audio/AudioFader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFader : MonoBehaviour {
+
+	float changesPerSecond;
+	float onTimeChange;
+	int fadeValue;
+
+	// Use this for initialization
+	void Start () {
+		
+
+		fadeValue = Fade;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void fadeOut() {
+		changesPerSecond = Volumef/Fade;
+		onTimeChange = 1/changesPerSecond;
+		while (fadeValue >= 1) {
+			AudioController.Instance.SetMusicVolume (Volumef-1);
+			fadeValue= fadeValue-1;
+			WaitForSeconds (onTimeChange);
+		}
+
+			AudioController.Instance.SetMusicVolume (0);
+
+
+
+	}
+
+}
