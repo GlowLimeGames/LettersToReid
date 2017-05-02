286db7b990bd0221ba279a0e94f01ae5b210209a
Maia Doerner <wc7642@gmail.com>
2017-05-01 21:57:48 -0400

    stopping gameplay music

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -344,11 +344,13 @@ public class AudioController : Controller, IAudioController
     		}
     		for (int index = 1; index < 10; index++) {
     			if (MapController.Instance.PeekMap().MapName == "MP-" + index) {
-    				EventController.Event("play_music_gameplay_0"+ index);
+					EventController.Event("stop_gameplay_music");
+					EventController.Event("play_music_gameplay_0"+ index);
     			}
     		}
     		if (MapController.Instance.PeekMap().MapName == "MP-10") {
-    			EventController.Event("play_music_gameplay_10");
+				EventController.Event("stop_gameplay_music");
+				EventController.Event("play_music_gameplay_10");
     		}
     		SceneMusicPlaying = false;
         }
--- Assets/Scripts/UI/SceneLoadingButton.cs
+++ Assets/Scripts/UI/SceneLoadingButton.cs
@@ -25,6 +25,11 @@ public class SceneLoadingButton : MonoBehaviour
     		SceneManager.LoadScene(this.sceneName);
     		EventController.Event("stop_mainmenu");
     		AudioController.Instance.StartCoroutine (AudioController.Instance.musicShift());
+			if (MapController.Instance.PeekMap ().MapName == "MapSelection") {
+				EventController.Event("stop_beginningamb");
+				EventController.Event("stop_endingamb");
+				EventController.Event("stop_gameplay_music");
+			}
         }
 	}
 
