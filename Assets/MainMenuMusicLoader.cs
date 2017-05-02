284fe370e13c537959d2eee236bb91d216170a13
Maia Doerner <wc7642@gmail.com>
2017-05-01 20:06:17 -0400

    Transition worked

--- Assets/MainMenuMusicLoader.cs
+++ Assets/MainMenuMusicLoader.cs
@@ -7,6 +7,8 @@ public class MainMenuMusicLoader : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		EventController.Event("play_music_menu_main_theme");
+		EventController.Event ("stop_beginningamb");
+		EventController.Event ("stop_endingamb");
 	}
 	
 	// Update is called once per frame
